7507b8a891f4c8d936089775c7692e9e7a7584af
Joseph Newman <josephnewman@Josephs-MacBook-Pro-2.local>
2023-12-23 21:24:35 -0600

    added two screens and functionality in javascript

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-// const btn1 = document.getElementById("btn1");
-// const btn2 = document.getElementById("btn2");
-// const btn3 = document.getElementById("btn3");
-// const btnPlus = document.getElementById("btn+");
-// const btn4 = document.getElementById("btn4");
-// const btn5 = document.getElementById("btn5");
-// const btn6 = document.getElementById("btn6");
-// const btnMinus = document.getElementById("btn-");
-// const btn7 = document.getElementById("btn7");
-// const btn8 = document.getElementById("btn8");
-// const btn9 = document.getElementById("btn9");
-// const btnMultiply = document.getElementById("btn*");
-// const btnClear = document.getElementById("btnC");
-// const btn0 = document.getElementById("btn0");
-// const btnEqual = document.getElementById("btn=");
-// const btnDivide = document.getElementById("btn/");
 
 const btnNum = document.querySelectorAll("[data-number]")
 const btnOperator = document.querySelectorAll("[data-operator]")
@@ -21,26 +5,41 @@ const btnClear = document.querySelector('[data-clear]')
 const btnEqual = document.querySelector('[data-equal]')
 
 const screen = document.querySelector('.screen')
+const screen2 = document.querySelector('.screen2')
+
 
 btnNum.forEach(button => {
     button.addEventListener('click', () => {
+        if (screen2.innerText != "") {
+            screen2.innerText = ""
+            screen.append(`${button.innerText}`)
+        } else {
         screen.append(`${button.innerText}`)
         console.log("num press")
+        }
     })
 })
 
 btnOperator.forEach(button => {
     button.addEventListener("click", () => {
+        if (screen2.innerText != "") {
+            screen.innerText = screen2.innerText;
+            screen.append(` ${button.innerText} `);
+            screen2.innerText = ""
+        } else {
         screen.append(` ${button.innerText} `)
+        }
     })
 })
 
 btnClear.addEventListener('click', () => {
     screen.innerText = ""
+    screen2.innerText = ""
 })
 
 btnEqual.addEventListener('click', () => {
-    screen.innerText = eval(screen.innerText);
+    screen2.innerText = eval(screen.innerText);
+    screen.innerText = ""
     console.log(screen)
 })
 
